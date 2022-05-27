470918da21c7d541208e7984571b7a3531fd5677
Cj Corp Dev <69812733+cjcorpdev@users.noreply.github.com>
2022-05-27 16:40:10 -0500

    actualizacion archivos modulo 5

---
+++ objetos/object.ts
@@ -0,0 +1,29 @@
+(() => {
+  let flash: {
+    name: string;
+    age: number;
+    powers: string[];
+    getName?: () => string;
+  } = {
+    name: "Barry Allen",
+    age: 24,
+    powers: ["super Velocidad", "Viajar en el tiempo"],
+  };
+  let SUPERMAN: {
+    name: string;
+    age: number;
+    powers: string[];
+    getName?: () => string;
+  } = {
+    name: "Clark kent",
+    age: 60,
+    powers: ["Super Fuerza"],
+  };
+  // flash = {
+  //   name: 'Clark Kent',
+  //   age: 60,
+  //   powers: ['Super Fuerza'],
+  //   getName(){ return this.name; },
+  // }
+  // console.log(flash.getName());
+})();
---
+++ objetos/type.ts
@@ -0,0 +1,22 @@
+(() => {
+  type Hero = {
+    name: string;
+    age?: number;
+    powers: string[];
+    getName?: () => string;
+  };
+
+  let flash: Hero = {
+    name: "Barry Allen",
+    age: 24,
+    powers: ["super Velocidad", "Viajar en el tiempo"],
+  };
+  let superman: Hero = {
+    name: "Clark kent",
+    age: 60,
+    powers: ["Super Fuerza"],
+    getName() {
+      return this.name;
+    },
+  };
+})();
